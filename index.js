1c78518fd1f99dc7634f6826b2b92eabd05a8d5b
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-10-16 18:35:52 -0400

    installed express-session dependency and added cookies session to project. MVP complete

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const helmet = require('helmet');
 const bcrypt = require('bcryptjs');
+const sessions = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(sessions);
 
-// const db = require('./database/dbConfig.js');
+const db = require('./database/dbConfig.js');
 const Users = require('./users-model.js');
 
 const server = express();
+
+const sessionConfiguration = {
+    name: 'ohfosho',
+    secret: 'keep it secret, keep it safe!',
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+    },
+    resave: false,
+    saveUninitialized: true,
+
+    store: new KnexSessionStore({
+        knex: db,
+        createtable: true,
+        clearInterval: 1000 * 60 * 30,
+    }),
+};
+
+server.use(sessions(sessionConfiguration));
 
 server.use(helmet());
 server.use(express.json());
@@ -62,25 +84,61 @@ server.post('/api/register', (req, res) => {
 server.post('/api/login', (req, res) => {
     let {username, password} = req.body;
 
-    if (username && password) {
-        Users.findBy({ username })
-            .first()
-            .then(user => {
-                if (user && bcrypt.compareSync(password, user.password)) {
-                    res.status(200).json({ message: `Welcome, ${user.username}.`});
-                } else {
-                    res.status(401).json({ message: 'You shan\'t pass!' });
-                }
-            })
-            .catch(error => {
-                res.status(500).json(error);
-            });
+    console.log('session', req.session);
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.username = user.username;
+
+                console.log('session', req.session);
+                res.status(200).json({
+                    message: `Welcome ${user.username}!`,
+                });
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+
+    // if (username && password) {
+    //     Users.findBy({ username })
+    //         .first()
+    //         .then(user => {
+    //             if (user && bcrypt.compareSync(password, user.password)) {
+    //                 res.status(200).json({ message: `Welcome, ${user.username}.`});
+    //             } else {
+    //                 res.status(401).json({ message: 'You shan\'t pass!' });
+    //             }
+    //         })
+    //         .catch(error => {
+    //             res.status(500).json(error);
+    //         });
+    // } else {
+    //     res.status(400).json({ message: 'Please provide credentials' });
+    // }
+});
+
+server.get('/api/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(error => {
+            res
+                .status(200)
+                .json({
+                    message:
+                        'You can check out any time you like, but you can never leave!',
+                });
+        });
     } else {
-        res.status(400).json({ message: 'Please provide credentials' });
+        res.status(200).json({ message: 'Already logged out' });
     }
 });
 
 server.get('/api/users', protected, (req, res) => {
+    console.log('username', req.session.username);
     Users.find()
         .then(users => {
             res.json(users);
